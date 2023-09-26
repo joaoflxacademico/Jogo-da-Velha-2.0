20bb8566334546ef3093826d9f6afc6524ee478a
joaoflxacademico <145574372+joaoflxacademico@users.noreply.github.com>
2023-09-26 16:54:46 -0300

    Update do Math.random com Math.floor script.js

--- script.js
+++ script.js
@@ -32,19 +32,20 @@ const simbolo5 = "o"
 const simbolo6 = "%"
 
 const simbolos = (num) =>{switch (num) {// Função auxiliar que irá definir qual é o simbolo sorteado pelo 'Math.random'
-  case 1: return simbolo1
-  case 2: return simbolo2
-  case 3: return simbolo3
-  case 4: return simbolo4
-  case 5: return simbolo5
-  case 6: return simbolo6
+  case 0: return simbolo1
+  case 1: return simbolo2
+  case 2: return simbolo3
+  case 3: return simbolo4
+  case 4: return simbolo5
+  case 5: return simbolo6
 }
 }
 const turno = (simbolo1,simbolo2,cont) =>{ /**  Função que irá utilizar da recursividade para repetir os turnos com os simbolos diferentes,
   esses que serão sorteados pela função auxiliar 'simbolos' */ 
   if (cont=4){
-    const num1 = Math.random(1,3)//Utilização da função 'Math.random' para randomizar as escolhas do simbolos.
-    const num2 = Math.random(4,6)
+    const num1 = Math.floor(Math.random()*3)/**Através dos número gerados entre 0 e 1 pelo Math.random utilizamos de uma multiplicação por 3 para deifinir os números entre 0 e 2 ou 3 e 5,
+     então usamos do Math.floor para aproximar o resultado da multiplicação ao valor mais próximo.*/
+    const num2 = Math.floor(Math.random()*3)
     return turno(simbolos(num1),simbolos(num2),cont=0)}//Retorno de uma nova rodada, após duas rodadas, com novos simbolos e o seu contador zerado.
     else{
       return turno(simbolo1,simbolo2,cont/**função de rodada, ou função clique*/)
