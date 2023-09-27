701a64f27b3cccbb07efc7675393967ef1d21bdb
joaoflxacademico <joao.flx@academico.ufs.br>
2023-09-27 11:28:25 -0300

    Início da construção da função jogada_da_vez

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 //Criação dos simbolos utilizados no jogo
-const simbolo = ['x','!','$','o','?','%']
+const simbolo = ['x','!','$','o','?','%']//Criação dos simbolos que serão utilizados no jogo.
+const atual = document.querySelector(".atual")//Criação de uma constante por meio da busca com o 'querySelector' dentro do document.
 
-const simbolos = (num) =>{switch (num) {// Função auxiliar que irá definir qual é o simbolo sorteado pelo 'Math.random'
+atual.innerHTML = `ATUAL JOGADA: ${simbolo[0]}`//Representação de qual jogador detém a atual jogada.
+
+const jogada_da_vez = (jogada,simboloA,simboloB) =>{//Função com o objetivo de alterenar o simbolo da jogada atual.
+  if(jogada==simboloA){return simboloB}
+  else{return simboloA} 
+}
+
+const simbolos = (num) =>{switch (num) {// Função auxiliar que irá selecionar qual é o simbolo sorteado pelo 'Math.random'
   case 0: return simbolo[0]
   case 1: return simbolo[1]
   case 2: return simbolo[2]
@@ -10,14 +18,15 @@ const simbolos = (num) =>{switch (num) {// Função auxiliar que irá definir qu
   case 5: return simbolo[5]
 }
 }
-const turno = (simbolo[0],simbolo[3],cont) =>{ /**  Função que irá utilizar da recursividade para repetir os turnos com os simbolos diferentes,
-  esses que serão sorteados pela função auxiliar 'simbolos' */ //Essa função já inicia o jogo com os simbolos 'x' e 'o'
+
+const turno = (simboloA=simbolo[0],simboloB=simbolo[3],cont=0) => { /**  Função que irá utilizar da recursividade para repetir os turnos com os simbolos diferentes,
+  esses que serão sorteados pela função auxiliar 'simbolos' */ //Essa função já inicia o jogo com os simbolos 'x' e 'o', isso pode ser alterado posteriormente.
   if (cont=4){
     const num1 = Math.floor(Math.random()*3)//Utilização da função 'Math.random' para randomizar as escolhas do simbolos.
     const num2 = Math.floor(Math.random()*3)+3
     return turno(simbolos(num1),simbolos(num2),cont=0)}//Retorno de uma nova rodada, após duas rodadas, com novos simbolos e o seu contador zerado.
     else{
-      return turno(simbolo[0],simbolo[3],cont+1/**função de rodada, ou função clique*/)
+      return turno(simboloA,simboloB,cont+1/**função de rodada, ou função clique*/)
     }
 }
 /*const simbolos = ["x", "$", "!", "?", "o", "%"]
